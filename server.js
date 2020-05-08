137a6e63e98a944024b77b9e75121ad979d405db
1stmuse <olysegs@gmail.com>
2020-05-08 21:28:58 +0100

    change

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname,"public/build")))
 
-const uri= 'mongodb+srv://muse_1st:akinn@cluster0-d1txe.mongodb.net/test?retryWrites=true&w=majority';
+// const uri= 'mongodb+srv://muse_1st:akinn@cluster0-d1txe.mongodb.net/test?retryWrites=true&w=majority';
+const uri=process.env.ATLAS_URI
 mongoose.connect(uri, {useNewUrlParser:true, useUnifiedTopology:true});
 const connection=mongoose.connection;
 connection.once('open', ()=>{
